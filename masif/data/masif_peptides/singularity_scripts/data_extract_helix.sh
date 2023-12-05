6486cd8ae033577e296b768c66929ab2cc2f7c7d
sad-noob <49701001+noobnewcomer@users.noreply.github.com>
2023-12-05 19:46:20 +0800

    Add files via upload

---
+++ masif/data/masif_peptides/singularity_scripts/data_extract_helix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+i=1
+while read p; do
+	if [ $(( i % 12 )) == $1 ]; then
+        	FIELD1=$(echo $p| cut -d" " -f1)
+        	PDBID=$(echo $FIELD1| cut -d"_" -f1)
+        	CHAIN1=$(echo $FIELD1| cut -d"_" -f2)
+        	./data_extract_helix_one.sh $PDBID\_$CHAIN1
+	fi
+	i=$((i+1))
+done < lists/bc-100-list.txt
